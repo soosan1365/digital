62bd3777aa99654ae5eab60812452fe33e7051c4
soosan <soosan.adh@gmail.com>
2025-09-12 17:27:03 +0330

    footer

--- src/components/Home/Helper/Title.tsx
+++ src/components/Home/Helper/Title.tsx
@@ -4,7 +4,7 @@ const Title = (props:{ title: string }) => {
   return (
     <div
       className=" w-full  h-[10vw] mx-auto flex justify-center items-center 
-    text-xl md:text-2xl lg:text-3xl xl:text-4xl font-extrabold  mt-10 
+    text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold  mt-10 
     bg-gradient-to-t bg-clip-text text-transparent from-[#55af69] to-[rgb(98,166,187)] "
     >
       {props.title}
